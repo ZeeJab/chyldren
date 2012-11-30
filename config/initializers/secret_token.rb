87e68becd7717e4254eb4b5260cee6cbfd7f0b3c
Zahra Jabini <zahra.arch@gmail.com>
2012-11-30 10:45:43 -0500

    initial app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chyldren::Application.config.secret_token = '909c6565387ca866d7aad593f6f5f70ce4aed0a3a9ceec8545f561a35a9c89ca76508dc723b0da12e260ed06738ae050d4225fe377b34d5327343f314537dce1'
